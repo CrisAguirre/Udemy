3b9a2c624da29131bd04f8211056e471a8ebc0aa
Cris <krontroth@gmail.com>
2023-11-18 12:24:05 -0500

    estilos, routing, componentes etc.

--- cliente/src/app/app.module.ts
+++ cliente/src/app/app.module.ts
@@ -4,10 +4,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { CrearProductoComponent } from './components/crear-producto/crear-producto.component';
+import { ListarProductosComponent } from './components/listar-productos/listar-productos.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CrearProductoComponent,
+    ListarProductosComponent
   ],
   imports: [
     BrowserModule,
