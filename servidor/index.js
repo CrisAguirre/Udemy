65a0600097fe6e548ba4112bad5b725a30a1c73d
Cris <krontroth@gmail.com>
2023-11-22 14:49:27 -0500

    config server

--- servidor/index.js
+++ servidor/index.js
@@ -1,1 +1,11 @@
-console.log("Holaaa Goonorreaaa fokiu menn");
+const express = require('express');
+//Creamos server
+const app = express();
+//Ruta Pcipal
+app.get('/', (req, res)=>{
+    res.send('Hola Gono');
+})
+
+app.listen(4000, ()=>{
+    console.log("Server is Running MTF!");
+})
