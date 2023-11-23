1719e7ca1b95c97b0f8f31801839049f9d42a721
Cris <krontroth@gmail.com>
2023-11-22 19:06:33 -0500

    config rutas en server

--- servidor/index.js
+++ servidor/index.js
@@ -6,11 +6,12 @@ const app = express();
 
 //Conectamos la BD
 conectarDB();
+app.use('/api/productos', require('./routes/producto'));
 
 //Ruta Pcipal
-app.get('/', (req, res)=>{
-    res.send('Hola Gono');
-})
+//app.get('/', (req, res)=>{
+  //  res.send('Hola Gono');
+//})
 
 app.listen(4000, ()=>{
     console.log("Server is Running MTF!");
